0bfab28abafc4dee062ad98663769ac00aed07d4
Bob W. Hogg <rwhogg@linux.com>
2017-10-21 08:42:19 -0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+rm -f moz-git-tools.deb
+mkdir -p usr/share/doc/moz-git-tools
+mkdir -p usr/bin
+cp moz-git-tools/{LICENSE,README.markdown} usr/share/doc/moz-git-tools
+cp moz-git-tools/git* usr/bin
+fpm -s dir -t deb -p moz-git-tools.deb -n moz-git-tools-unofficial -d git -m "Bob W. Hogg" usr
