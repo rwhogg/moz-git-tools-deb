d2a5c5a3253a53dd0087d645ade0527ba1757f70
Bob W. Hogg <rwhogg@linux.com>
2017-10-21 08:53:53 -0700

    Fixup
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ rm -f moz-git-tools.deb
 mkdir -p usr/share/doc/moz-git-tools
 mkdir -p usr/bin
 cp moz-git-tools/{LICENSE,README.markdown} usr/share/doc/moz-git-tools
+cp GPL-2 usr/share/doc/moz-git-tools
 cp moz-git-tools/git* usr/bin
 fpm -s dir -t deb -p moz-git-tools.deb -n moz-git-tools-unofficial -d git -m "Bob W. Hogg" usr
