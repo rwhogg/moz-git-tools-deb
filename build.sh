dff3f952f4274909961bec4321321961b91a76a9
Bob W. Hogg <rwhogg@linux.com>
2018-01-16 19:37:44 -0800

    Remove git-root to avoid conflict with git-extras
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ mkdir -p usr/bin
 cp moz-git-tools/{LICENSE,README.markdown} usr/share/doc/moz-git-tools
 cp GPL-2 usr/share/doc/moz-git-tools
 cp moz-git-tools/git* usr/bin
+rm -f usr/bin/git-root
 fpm -s dir -t deb -p moz-git-tools.deb -n moz-git-tools-unofficial -d git -m "Bob W. Hogg" usr
